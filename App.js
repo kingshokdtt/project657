62908955fb409f996a9a27f3d9525436a07201d7
Kingshok <kingshok.datta@student.csulb.edu>
2022-06-18 21:53:22 -0700

    project657

--- App.js
+++ App.js
@@ -11,11 +11,15 @@ export default function App() {
   function startAddGoalHandler () {
     setModalIsVisible(true);
   }
+  function endAddGoalHandler () {
+    setModalIsVisible(false);
+  }
   function addGoalHandler(enteredGoalText) {
     setCourseGoals(currentCourseGoals => [
       ...currentCourseGoals,
       {text: enteredGoalText, id: Math.random().toString()},
     ]);
+    endAddGoalHandler();
   }
 
   function deleteGoalHandler(id) {
@@ -28,7 +32,7 @@ export default function App() {
   return (
     <View style={styles.appContainer}>
       <Button title='Add New Goal' color="gray" onPress={startAddGoalHandler}/>
-      <GoalInput visible={modalIsVisible} onAddGoal ={addGoalHandler} />   
+      <GoalInput visible={modalIsVisible} onAddGoal ={addGoalHandler} onCancel={endAddGoalHandler}/>   
       <View style={styles.goalsContainer}>
 
         <FlatList 
